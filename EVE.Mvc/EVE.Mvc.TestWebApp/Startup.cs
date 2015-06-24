52344b18502c9a1bd6b3fdd85e41ee584c299614
Peter Balogh <peter-balogh@outlook.com>
2015-06-24 23:08:57 +0200

    beggining razor views tutorial

---
+++ EVE.Mvc/EVE.Mvc.TestWebApp/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin.FileSystems;
+using EVE.Mvc;
+using Microsoft.Owin.Extensions;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Optimization;
+using System.IO;
+using EVE.Mvc.Plugin;
+
+[assembly: OwinStartupAttribute(typeof(EVE.Mvc.TestWebApp.Startup))]
+namespace EVE.Mvc.TestWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+           // ViewEngines.Engines.Add(new EmbeddedViewEngine());
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+          
+
+            ConfigureAuth(app);
+            //ConfigureEmbeddedFileSystem(app);
+
+            app.UseEmbeddedPlugins("Plugins");
+            
+
+           
+        }
+
+       
+    }
+}
---
+++ EVE.Mvc/EVE.Mvc/Composition/AppMefContainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Compilation;
+using System.Web.Hosting;
+
+namespace EVE.Mvc.Composition
+{
+    public static class AppMefContainer
+    {
+
+        public static CompositionContainer Container {get; private set;}
+
+        static AppMefContainer()
+        {
+            if (AppDomain.CurrentDomain == null)
+                throw new ApplicationException("AppDomain does not exist. AppDomain is needed in order to create MEF container.");
+            
+            DirectoryCatalog dcat = new DirectoryCatalog(Path.Combine(HostingEnvironment.ApplicationPhysicalPath,"bin"));
+           
+            //var asmCatalogs = from a in BuildManager.GetReferencedAssemblies().Cast<Assembly>()
+            //                  select (new AssemblyCatalog(a));
+            
+           
+            //AggregateCatalog aggregateCatalog = new AggregateCatalog(asmCatalogs);
+            Container = new CompositionContainer(dcat);
+        }
+    }
+}
