9d849d2a998909e5647e952cb187974662ef7e47
Peter Balogh <peter-balogh@outlook.com>
2019-09-24 15:59:13 +0200

    1

---
+++ EV5.Mvc/EV5.Mvc/EV5.Mvc.Samples.ViewEngine/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EV5.Mvc.Samples.ViewEngine
+{
+    public class Class1
+    {
+    }
+}
--- EV5.Mvc/EV5.Mvc/EV5.Mvc.TestWebApp/Startup.cs
+++ EV5.Mvc/EV5.Mvc/EV5.Mvc.TestWebApp/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace EV5.Mvc.TestWebApp
 {
@@ -21,29 +22,31 @@ namespace EV5.Mvc.TestWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
